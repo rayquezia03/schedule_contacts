805068e6cb9471ce6dd1088dc03593d43bacb07a
rayane <rayquezia03@gmail.com>
2023-02-21 22:28:14 -0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  resources :contacts
+  resources :users, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   #nome do controler + nome da action
   root to: "static_pages#index"
-  get 'Sobre',    to: 'static_pages#sobre'
-  get 'Contato',  to: 'static_pages#contato'
+  get 'sobre',    to: 'static_pages#sobre'
+  
+  get 'cadastro', to: 'users#new'
+  get 'entrar',   to: 'sessions#new'
+  post 'entrar',  to: 'sessions#create'
+  delete 'sair',  to: 'sessions#destroy'
 end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_003448) do
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "password_digest", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
